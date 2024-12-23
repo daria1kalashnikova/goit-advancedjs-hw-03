0c4cacbe435a0544bad5259fd76eebb9b4ef6c57
daria1kalashnikova <dk.kalashnikova@gmail.com>
2024-12-23 15:19:22 +0000

    Deploying to gh-pages from @ daria1kalashnikova/goit-advancedjs-hw-03@17b51d9271d32331264c929ce8447b5cf5186d98 🚀

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const a=document.querySelector(".js-search-form"),c=t=>`<li class="gallery-card">
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&o(r)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const a=document.querySelector(".js-search-form"),c=document.querySelector(".js-gallery"),l=t=>`<li class="gallery-card">
               <img
                 class="gallery-img"
                 src="${t.webformatURL}"
                 alt="${t.tags}"
-                width="360"
-                height="200"
               />
               <div class="img-info">
                 <ul class="img-info-list">
@@ -23,5 +21,5 @@
                   <li class="img-info-item">
         <span class="img-info-title" >Downloads</span>
         <span class="img-info-number">${t.downloads}</span>
-                  </li>`,l=t=>{t.preventDefault();const o=t.currentTarget.elements.user_query.value;console.log(o),fetch(`https://pixabay.com/api/?key=44900327-ed76a6d24ccf74b976808d529&q=${o}&image_type=photo&orientation=horizontal&safesearch=true`).then(i=>{if(!i.ok)throw new Error(i.status);return i.json()}).then(i=>{console.log(i);const n=i.hits.map(e=>{c(e)});console.log(n)}).catch(i=>{console.log(i)})};a.addEventListener("submit",l);
+                  </li>`,m=t=>{t.preventDefault();const n=t.currentTarget.elements.user_query.value;console.log(n),fetch(`https://pixabay.com/api/?key=44900327-ed76a6d24ccf74b976808d529&q=${n}&image_type=photo&orientation=horizontal&safesearch=true`).then(i=>{if(!i.ok)throw new Error(i.status);return i.json()}).then(i=>{console.log(i);const o=i.hits.map(e=>{l(e)}).join("");c.innerHTML=o}).catch(i=>{console.log(i)})};a.addEventListener("submit",m);
 //# sourceMappingURL=index.js.map
