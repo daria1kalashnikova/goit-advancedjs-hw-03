83d57f63c34b16ceac9525a32d7a90d1342b7f0d
daria1kalashnikova <dk.kalashnikova@gmail.com>
2024-12-23 18:59:59 +0000

    Deploying to gh-pages from @ daria1kalashnikova/goit-advancedjs-hw-03@a0682ac82e6eb43753b38ef11df7081fce9445d8 🚀

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&o(r)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const a=document.querySelector(".js-search-form"),l=document.querySelector(".js-gallery"),c=t=>`<li class="gallery-card">
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const c=document.querySelector(".js-search-form"),a=document.querySelector(".js-gallery"),l=document.querySelector(".js-loader"),m=i=>`<li class="gallery-card">
               <img
                 class="gallery-img"
-                src="${t.webformatURL}"
-                alt="${t.tags}"
+                src="${i.webformatURL}"
+                alt="${i.tags}"
               />
               <div class="img-info">
                 <ul class="img-info-list">
              <li class="img-info-item">
         <span class="img-info-title" >Likes</span>
-        <span class="img-info-number">${t.likes}</span>
+        <span class="img-info-number">${i.likes}</span>
                   </li>
                   <li class="img-info-item">
         <span class="img-info-title" >Views</span>
-        <span class="img-info-number">${t.views}</span>
+        <span class="img-info-number">${i.views}</span>
                   </li>
                   <li class="img-info-item">
         <span class="img-info-title" >Comments</span>
-        <span class="img-info-number">${t.comments}</span>
+        <span class="img-info-number">${i.comments}</span>
                   </li>
                   <li class="img-info-item">
         <span class="img-info-title" >Downloads</span>
-        <span class="img-info-number">${t.downloads}</span>
+        <span class="img-info-number">${i.downloads}</span>
                   </li>
                   </ul>
                   </div>
-                  </li>`,m=t=>{t.preventDefault();const n=t.currentTarget.elements.user_query.value;console.log(n),fetch(`https://pixabay.com/api/?key=44900327-ed76a6d24ccf74b976808d529&q=${n}&image_type=photo&orientation=horizontal&safesearch=true`).then(i=>{if(!i.ok)throw new Error(i.status);return i.json()}).then(i=>{console.log(i);const o=i.hits.map(e=>c(e)).join("");l.innerHTML=o}).catch(i=>{console.log(i)})};a.addEventListener("submit",m);
+                  </li>`,u=i=>{i.preventDefault();const r=i.currentTarget.elements.user_query.value;a.innerHTML="",l.classList.remove("is-hidden"),fetch(`https://pixabay.com/api/?key=44900327-ed76a6d24ccf74b976808d529&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`).finally(()=>{l.classList.add("is-hidden")}).then(s=>{if(!s.ok)throw new Error(s.status);return s.json()}).then(s=>{if(console.log(s),s.total===0){alert("Sorry, there are no images matching your search query. Please try again!"),a.innerHTML="";return}const n=s.hits.map(e=>m(e)).join("");a.innerHTML=n}).catch(s=>{console.log(s)})};c.addEventListener("submit",u);
 //# sourceMappingURL=index.js.map
