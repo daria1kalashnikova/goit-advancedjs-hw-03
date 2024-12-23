a0682ac82e6eb43753b38ef11df7081fce9445d8
daria1kalashnikova <dk.kalashnikova@gmail.com>
2024-12-23 20:59:37 +0200

    add loader

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const formEl = document.querySelector('.js-search-form');
 const galleryElement = document.querySelector('.js-gallery');
+const loaderEl = document.querySelector('.js-loader');
 
 const createGalleryCardTemplate = galleryEl => {
   return `<li class="gallery-card">
@@ -34,11 +35,15 @@ const createGalleryCardTemplate = galleryEl => {
 const onFormSubmit = event => {
   event.preventDefault();
   const inputValue = event.currentTarget.elements.user_query.value;
-  console.log(inputValue);
+  galleryElement.innerHTML = '';
+  loaderEl.classList.remove('is-hidden');
 
   fetch(
     `https://pixabay.com/api/?key=44900327-ed76a6d24ccf74b976808d529&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
   )
+    .finally(() => {
+      loaderEl.classList.add('is-hidden');
+    })
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
@@ -47,6 +52,16 @@ const onFormSubmit = event => {
     })
     .then(data => {
       console.log(data);
+      if (data.total === 0) {
+        alert(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+
+        galleryElement.innerHTML = '';
+
+        return;
+      }
+
       const galleryCardsTemplate = data.hits
         .map(el => {
           return createGalleryCardTemplate(el);
